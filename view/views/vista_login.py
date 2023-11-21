db58e3ce86816ebb029cf984d91f1bd587a6d3c3
kaz <fabri.r.nm@gmail.com>
2023-11-21 18:37:27 -0300

    alertas de login mejoradas

--- view/views/vista_login.py
+++ view/views/vista_login.py
@@ -25,24 +25,24 @@ class VistaLogin(VistaPrincipal):
         alerta_fuente = ctk.CTkFont(size=12)
         self.alerta_label = ctk.CTkLabel(frame_principal, font=alerta_fuente, text_color="red")
 
-        datos_frame = ctk.CTkFrame(frame_principal, fg_color="transparent")
-        datos_frame.rowconfigure(0, weight=1)
-        datos_frame.columnconfigure(0, weight=1)
-        datos_frame.pack()
+        self.frame_datos = ctk.CTkFrame(frame_principal, fg_color="transparent")
+        self.frame_datos.rowconfigure(0, weight=1)
+        self.frame_datos.columnconfigure(0, weight=1)
+        self.frame_datos.pack()
 
-        self.nombre_usuario_label = ctk.CTkLabel(datos_frame, text="Usuario")
+        self.nombre_usuario_label = ctk.CTkLabel(self.frame_datos, text="Usuario")
         self.nombre_usuario_label.grid_configure(row=0, column=0, padx=5, pady=5)
         self.nombre_usuario_label.grid()
 
-        self.nombre_usuario_entry = ctk.CTkEntry(datos_frame)
+        self.nombre_usuario_entry = ctk.CTkEntry(self.frame_datos)
         self.nombre_usuario_entry.grid_configure(row=0, column=1, padx=5, pady=5)
         self.nombre_usuario_entry.grid()
 
-        self.contrasena_label = ctk.CTkLabel(datos_frame, text="Contraseña")
+        self.contrasena_label = ctk.CTkLabel(self.frame_datos, text="Contraseña")
         self.contrasena_label.grid_configure(row=1, column=0, padx=5, pady=5)
         self.contrasena_label.grid()
 
-        self.contrasena_entry = ctk.CTkEntry(datos_frame, show="*")
+        self.contrasena_entry = ctk.CTkEntry(self.frame_datos, show="*")
         self.contrasena_entry.grid_configure(row=1, column=1, padx=5, pady=5)
         self.contrasena_entry.grid()
 
@@ -70,9 +70,10 @@ class VistaLogin(VistaPrincipal):
         frame_tema.pack_configure(side="bottom", padx=5, pady=5)
         frame_tema.pack()
 
-        self.master.bind("<<DatosInvalidos>>", self._datos_invalidos)
-        self.master.bind("<<EnUso>>", self._en_uso)
-        self.master.bind("<<CamposVacios>>", self._campos_vacios)
+        self.master.bind("<<DatosInvalidos>>", self._alerta_datos_invalidos)
+        self.master.bind("<<EnUso>>", self._alerta_en_uso)
+        self.master.bind("<<CamposVacios>>", self._alerta_campos_vacios)
+        self.master.bind("<Key>", self._quitar_alerta)
         self.master.bind("<Return>", self._autenticar_usuario)
 
     def _cambiar_tema(self, *args):
@@ -91,14 +92,18 @@ class VistaLogin(VistaPrincipal):
         if self.controlador.registrar_usuario(username, password):
             self.controlador.ir_a_inicio()
 
-    def _datos_invalidos(self, *args):
+    def _alerta_datos_invalidos(self, *args):
         self.alerta_label.configure(text="Los datos ingresados no coinciden")
-        self.alerta_label.pack()
+        self.alerta_label.pack(after=self.frame_datos)
 
-    def _en_uso(self, *args):
+    def _alerta_en_uso(self, *args):
         self.alerta_label.configure(text="Nombre de usuario en uso")
-        self.alerta_label.pack()
+        self.alerta_label.pack(after=self.frame_datos)
 
-    def _campos_vacios(self, *args):
+    def _alerta_campos_vacios(self, *args):
         self.alerta_label.configure(text="Los campos no pueden estar vacios")
-        self.alerta_label.pack()
+        self.alerta_label.pack(after=self.frame_datos)
+
+    def _quitar_alerta(self, *args):
+        # Se puede comprobar si la label esta packeada con self.alerta_label.winfo_ismapped()
+        self.alerta_label.pack_forget()
